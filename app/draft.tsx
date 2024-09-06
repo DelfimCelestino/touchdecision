16344e404da90df1fc4e78e34c1afe68da72e076
Delfim Celestino <delfimcelestinoamissepastola@gmail.com>
2024-09-06 14:41:35 +0200

    feat: adding alternate colors animation

--- app/draft.tsx
+++ app/draft.tsx
@@ -1,185 +1,176 @@
-"use client";
-import { useState, useEffect, useRef } from "react";
+"use client"
+
+import CreatorDialog from "@/components/creator-dialog"
+import InfoModal from "@/components/info-modal"
+import InstallModal from "@/components/install-modal"
+import { Button } from "@/components/ui/button"
+import { CircleHelpIcon, Info } from "lucide-react"
+import { useState, useEffect, useRef } from "react"
 
 interface Touch {
-  x: number;
-  y: number;
-  id: number;
+  x: number
+  y: number
+  id: number
 }
 
 export default function FingerGame() {
-  const [touches, setTouches] = useState<Touch[]>([]); // Armazena as posições dos toques
-  const [selectedFinger, setSelectedFinger] = useState<Touch | null>(null); // O dedo escolhido no final
-  const [isChoosing, setIsChoosing] = useState<boolean>(false); // Controla se está no processo de escolha
-  const [colors, setColors] = useState<string[]>([]); // Armazena as cores de cada dedo
-  const [showStartMessage, setShowStartMessage] = useState<boolean>(true); // Exibe a mensagem inicial
-  const intervalRef = useRef<NodeJS.Timeout | null>(null);
-
-  const resetGame = () => {
-    setTouches([]);
-    setSelectedFinger(null);
-    setIsChoosing(false);
-    setColors([]);
-    setShowStartMessage(true);
-  };
+  const [touches, setTouches] = useState<Touch[]>([])
+  const [counter, setCounter] = useState<number>(5)
+  const [showStartMessage, setShowStartMessage] = useState<boolean>(true)
+  const [hasStarted, setHasStarted] = useState<boolean>(false)
+  const [selectedFinger, setSelectedFinger] = useState<Touch | null>(null)
+  const intervalRef = useRef<NodeJS.Timeout | null>(null)
+  const [openInfo, setOpenInfo] = useState(false)
+  const [open, setOpen] = useState(false)
+  const [isChoosing, setIsChoosing] = useState(false)
+  const [currentColor, setCurrentColor] = useState("bg-red-500")
 
   useEffect(() => {
-    const handleTouchStart = (e: TouchEvent) => {
-      const newTouches: Touch[] = Array.from(e.touches).map((touch) => ({
-        x: touch.pageX,
-        y: touch.pageY,
-        id: touch.identifier,
-      }));
-      setTouches(newTouches);
-      setColors(new Array(newTouches.length).fill("red")); // Inicializa todas as cores como vermelho
-
-      // Remove a mensagem de início assim que o primeiro toque for detectado
-      if (newTouches.length > 0 && showStartMessage) {
-        setShowStartMessage(false);
+    if ("serviceWorker" in navigator) {
+      const handleServiceWorker = async () => {
+        await navigator.serviceWorker.register("/service-worker.js")
       }
 
-      // Inicia o processo de escolha quando os dedos são mantidos na tela
-      if (!isChoosing && newTouches.length > 0) {
-        setIsChoosing(true);
-      }
-    };
+      handleServiceWorker()
+    }
+  }, [])
 
-    const handleTouchMove = (e: TouchEvent) => {
-      const updatedTouches: Touch[] = Array.from(e.touches).map((touch) => ({
-        x: touch.pageX,
-        y: touch.pageY,
-        id: touch.identifier,
-      }));
-      setTouches(updatedTouches);
-    };
-
-    const handleTouchEnd = (e: TouchEvent) => {
-      // Se todos os dedos forem removidos, reset o jogo
-      if (e.touches.length === 0) {
-        resetGame();
-      } else {
-        // Atualiza a lista de toques quando algum dedo for removido
-        const remainingTouches: Touch[] = Array.from(e.touches).map(
-          (touch) => ({
-            x: touch.pageX,
-            y: touch.pageY,
-            id: touch.identifier,
-          })
-        );
-        setTouches(remainingTouches);
-      }
-    };
-
-    document.addEventListener("touchstart", handleTouchStart);
-    document.addEventListener("touchmove", handleTouchMove);
-    document.addEventListener("touchend", handleTouchEnd);
-
-    return () => {
-      document.removeEventListener("touchstart", handleTouchStart);
-      document.removeEventListener("touchmove", handleTouchMove);
-      document.removeEventListener("touchend", handleTouchEnd);
-    };
-  }, [isChoosing, showStartMessage]);
-
-  // Alterna as cores enquanto está escolhendo o dedo
   useEffect(() => {
-    if (isChoosing && touches.length > 0) {
-      let counter = 0;
-
+    if (showStartMessage) {
       intervalRef.current = setInterval(() => {
-        // Alterna entre vermelho e verde
-        setColors((prevColors) =>
-          prevColors.map((color) => (color === "red" ? "green" : "red"))
-        );
-        counter++;
-
-        // Após 5 segundos (5000ms), para de alternar e seleciona o dedo
-        if (counter >= 10) {
-          if (intervalRef.current) clearInterval(intervalRef.current);
-          const randomIndex = Math.floor(Math.random() * touches.length);
-          setSelectedFinger(touches[randomIndex]);
-          setIsChoosing(false);
-        }
-      }, 500); // Alterna as cores a cada 500ms (meio segundo)
+        setCounter((prevCounter) => {
+          if (prevCounter <= 1) {
+            clearInterval(intervalRef.current!)
+            setShowStartMessage(false)
+            setHasStarted(true)
+            return 0
+          }
+          return prevCounter - 1
+        })
+      }, 1000)
     }
 
     return () => {
-      if (intervalRef.current) clearInterval(intervalRef.current);
-    };
-  }, [isChoosing, touches]);
+      if (intervalRef.current) clearInterval(intervalRef.current)
+    }
+  }, [showStartMessage])
+
+  useEffect(() => {
+    const handleTouchStart = (e: TouchEvent) => {
+      if (hasStarted || showStartMessage) {
+        const newTouches: Touch[] = Array.from(e.touches).map((touch) => ({
+          x: touch.pageX,
+          y: touch.pageY,
+          id: touch.identifier,
+        }))
+        setTouches(newTouches)
+      }
+    }
+
+    document.addEventListener("touchstart", handleTouchStart)
+
+    return () => {
+      document.removeEventListener("touchstart", handleTouchStart)
+    }
+  }, [hasStarted, showStartMessage])
+
+  useEffect(() => {
+    if (!showStartMessage && hasStarted && touches.length > 0) {
+      setIsChoosing(true)
+
+      const colorInterval = setInterval(() => {
+        setCurrentColor((prevColor) =>
+          prevColor === "bg-red-500" ? "bg-green-500" : "bg-red-500",
+        )
+      }, 500)
+
+      const chooseFinger = () => {
+        const randomIndex = Math.floor(Math.random() * touches.length)
+        setSelectedFinger(touches[randomIndex])
+        setIsChoosing(false)
+        clearInterval(colorInterval) // Para a troca de cores quando o dedo for escolhido
+      }
+
+      const chooseFingerTimeout = setTimeout(() => {
+        chooseFinger()
+        setHasStarted(false)
+      }, 3000) // Escolhe o dedo após 3 segundos
+
+      return () => {
+        clearTimeout(chooseFingerTimeout)
+        clearInterval(colorInterval)
+      }
+    }
+  }, [showStartMessage, hasStarted, touches])
+
+  const resetGame = () => {
+    setTouches([])
+    setSelectedFinger(null)
+    setHasStarted(false)
+    setShowStartMessage(true)
+    setCounter(5)
+    setCurrentColor("bg-red-500")
+  }
 
   return (
-    <div
-      style={{
-        position: "relative",
-        height: "100vh",
-        backgroundColor: "#f0f0f0",
-      }}
-    >
-      <h1 style={{ textAlign: "center" }}>Press Your Fingers!</h1>
+    <div className="absolute inset-0 flex items-center justify-center overflow-hidden bg-background">
+      <InstallModal />
+      <InfoModal open={openInfo} onOpenChange={setOpenInfo} />
+      <CreatorDialog open={open} onOpenChange={setOpen} />
+      <CircleHelpIcon
+        onClick={() => setOpenInfo(true)}
+        className="fixed left-4 top-4 z-30 h-6 w-6 text-zinc-50"
+      />
+      <Info
+        onClick={() => setOpen(true)}
+        className="fixed right-4 top-4 z-30 h-6 w-6 text-zinc-50"
+      />
 
-      {/* Renderiza os círculos dos dedos */}
-      {!selectedFinger &&
-        touches.map((touch, index) => (
-          <div
-            key={touch.id}
-            style={{
-              position: "absolute",
-              width: "200px",
-              height: "200px",
-              backgroundColor: colors[index] || "red", // Alterna a cor
-              borderRadius: "50%",
-              left: `${touch.x - 25}px`,
-              top: `${touch.y - 25}px`,
-            }}
-          />
-        ))}
+      {!showStartMessage && touches.length < 2 && (
+        <h1 className="text-center text-2xl">Pressionem seus dedos na tela</h1>
+      )}
 
-      {/* Exibe apenas o dedo selecionado com verde */}
-      {selectedFinger && (
-        <div
-          style={{
-            position: "absolute",
-            width: "200px",
-            height: "200px",
-            backgroundColor: "green",
-            borderRadius: "50%",
-            left: `${selectedFinger.x - 30}px`,
-            top: `${selectedFinger.y - 30}px`,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <p style={{ color: "#fff" }}>Selected!</p>
+      {showStartMessage && (
+        <div className="text-center text-2xl">
+          <p>Coloquem todos os dedos na tela!</p>
+          <p>{counter}</p>
         </div>
       )}
 
-      {/* Mensagem inicial */}
-      {showStartMessage && !isChoosing && !selectedFinger && (
-        <p style={{ textAlign: "center", color: "#888" }}>
-          Coloquem todos os dedos na tela para começar a escolha!
-        </p>
-      )}
+      {touches.map((touch, index) => (
+        <div
+          className={`absolute flex h-[200px] w-[200px] items-center justify-center rounded-full ${
+            isChoosing ? currentColor : "bg-red-500"
+          } text-2xl font-bold text-zinc-50`}
+          key={touch.id}
+          style={{
+            left: `${touch.x - 100}px`,
+            top: `${touch.y - 100}px`,
+          }}
+        >
+          {index + 1}
+        </div>
+      ))}
 
-      {/* Mensagem de escolha em andamento */}
-      {isChoosing && !selectedFinger && (
-        <p style={{ textAlign: "center", color: "#888" }}>
-          Escolhendo o dedo...
-        </p>
-      )}
-
-      {/* Botão de reiniciar */}
       {selectedFinger && (
-        <div style={{ textAlign: "center", marginTop: "20px" }}>
-          <button
-            onClick={resetGame}
-            style={{ padding: "10px 20px", fontSize: "16px" }}
-          >
+        <div
+          className="absolute flex h-[200px] w-[200px] items-center justify-center rounded-full bg-green-500 text-2xl font-bold text-zinc-50"
+          style={{
+            left: `${selectedFinger.x - 100}px`,
+            top: `${selectedFinger.y - 100}px`,
+          }}
+        >
+          <p>Selecionado!</p>
+        </div>
+      )}
+
+      {!showStartMessage && selectedFinger && (
+        <div className="absolute bottom-10 left-0 right-0 text-center">
+          <Button className="w-full" variant={"secondary"} onClick={resetGame}>
             Recomeçar
-          </button>
+          </Button>
         </div>
       )}
     </div>
-  );
+  )
 }
